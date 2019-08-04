7c1fef0b1ee7bf74b3881baae3152133c2153220
unknown <jeanned'arc@cchub.rw>
2019-08-04 15:40:49 +0200

    adding node version

--- config/database.js
+++ config/database.js
@@ -1,10 +1,10 @@
 'use strict'
 
-const Url = require('url-parse')
-const CLEARDB_DATABASE_URL = new Url(Env.get('CLEARDB_DATABASE_URL'))
-
 // const Url = require('url-parse')
-// const DATABASE_URL = new Url(Env.get('DATABASE_URL'))
+// const CLEARDB_DATABASE_URL = new Url(Env.get('CLEARDB_DATABASE_URL'))
+
+const Url = require('url-parse')
+const DATABASE_URL = new Url(Env.get('DATABASE_URL'))
 
 /** @type {import('@adonisjs/framework/src/Env')} */
 const Env = use('Env')
@@ -56,11 +56,11 @@ module.exports = {
   mysql: {
     client: 'mysql',
     connection: {
-      host: Env.get('DB_HOST', CLEARDB_DATABASE_URL.host),
+      host: Env.get('DB_HOST', DATABASE_URL.host),
       port: Env.get('DB_PORT', ''),
-      user: Env.get('DB_USER', CLEARDB_DATABASE_URL.username),
-      password: Env.get('DB_PASSWORD', CLEARDB_DATABASE_URL.password),
-      database: Env.get('DB_DATABASE', CLEARDB_DATABASE_URL.pathname.substr(1))
+      user: Env.get('DB_USER', DATABASE_URL.username),
+      password: Env.get('DB_PASSWORD', DATABASE_URL.password),
+      database: Env.get('DB_DATABASE', DATABASE_URL.pathname.substr(1))
     },
     debug: Env.get('DB_DEBUG', false)
   },
